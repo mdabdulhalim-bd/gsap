a6b50452aee10b4311c4c946dd879f910f06aa11
mdabdulhalim-bd <halim.zoomit@gmail.com>
2024-06-25 22:56:01 +0600

    custom cursor

---
+++ part-04/script.js
@@ -0,0 +1,31 @@
+
+let overlay = document.querySelector("#overlay");
+let cursor = document.querySelector("#cursor");
+
+
+
+window.addEventListener("mousemove", function(dets){
+    cursor.innerText = "View More"
+    gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y,
+        duration: 0.5,
+        
+    })
+})
+
+overlay.addEventListener("mouseleave", ()=>{
+    gsap.to(cursor,{
+        scale:0.5,
+        duration: 0.5,
+        ease: "elastic.out(1, 0.3)"
+       })
+})
+
+overlay.addEventListener("mouseenter", ()=>{
+   gsap.to(cursor,{
+    scale: 1.5,
+    duration: 0.5,
+    ease: "elastic.out(1, 0.3)"
+   })
+})
